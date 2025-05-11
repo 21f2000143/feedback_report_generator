e2c841c7e580e769f352546cc938c696a357aeeb
Sachin Kumar <pseudo-01@outlook.com>
2025-05-12 02:52:19 +0530

    added celery signal approach

--- reports/tasks.py
+++ reports/tasks.py
@@ -1,9 +1,10 @@
 from celery import shared_task
 from .models import Report
 from .utils import generate_report_card
-from celery.signals import task_postrun
+from celery.signals import task_success, task_failure
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
+import time
 
 
 def get_event_sorted_alis(events):
@@ -16,67 +17,62 @@ def get_event_sorted_alis(events):
     return event_order
 
 
-@task_postrun.connect
-def task_postrun_handler(sender=None, task_id=None, task=None, args=None,
-                         kwargs=None, retval=None, state=None, **extras):
-    task_id = task_id
-    report = Report.objects.filter(task_id=task_id).first()
-    if report:
-        report.status = state
-        if state == 'FAILURE' and retval:
-            report.error = str(retval)  # Store exception message
-        report.save()
+@task_success.connect
+def task_success_handler(sender=None, result=None, **kwargs):
+    task_id = sender.request.id
+    if result['type'] == 'pdf':
+        # Save the PDF report
+        Report.objects.create(
+            task_id=task_id,
+            status="SUCCESS",
+            pdf_content=result['pdf_file_link'])
+    elif result['type'] == 'html':
+        Report.objects.create(
+            task_id=task_id,
+            status="SUCCESS",
+            html_content=result['html_content'])
 
 
-@shared_task(autoretry_for=(Exception,), max_retries=3)
+@task_failure.connect
+def task_failure_handler(sender=None, task_id=None, exception=None,
+                         args=None, kwargs=None, traceback=None, einfo=None, **kwds):
+    Report.objects.create(
+        task_id=task_id,
+        status="FAILURE",
+        error=str(exception)
+    )
+
+
+@shared_task(autoretry_for=(Exception,), max_retries=3, retry_backoff=True)
 def generate_html_report(data):
     # Parse events
-    try:
-        context = []
-        for each in data:
-            dic_obj = {}
-            dic_obj['student_id'] = each['student_id']
-            event_order = get_event_sorted_alis(each['events'])
-            dic_obj['event_order'] = event_order
-            context.append(dic_obj)
-        # Wrap the list in a dictionary for the template
-        full_context = {'full_context': context}
-        # Generate HTML directly without template file
-        html_report = mark_safe(render_to_string('report.html', full_context))
-        report = Report.objects.create(
-            task_id=generate_html_report.request.id, html_content=html_report)
-        return report.id
-    except Exception as e:
-        error_message = str(e)
-        report = Report.objects.create(
-            task_id=generate_html_report.request.id,
-            status="FAILURE"
-        )
-        return error_message
+    context = []
+    for each in data:
+        dic_obj = {}
+        dic_obj['student_id'] = each['student_id']
+        event_order = get_event_sorted_alis(each['events'])
+        dic_obj['event_order'] = event_order
+        context.append(dic_obj)
+    # Wrap the list in a dictionary for the template
+    full_context = {'full_context': context}
+    # Generate HTML directly without template file
+    html_report = mark_safe(render_to_string('report.html', full_context))
+    return {"html_report": html_report, "type": "html"}
 
 
-@shared_task(autoretry_for=(Exception,), max_retries=3)
+@shared_task(autoretry_for=(Exception,), max_retries=3, retry_backoff=True)
 def generate_pdf_report(data):
-    try:
-        # Parse events and prepare data
-        summary = []
-        for each in data:
-            dic_obj = {}
-            dic_obj['student_id'] = each['student_id']
-            event_order = get_event_sorted_alis(each['events'])
-            dic_obj['event_order'] = event_order
-            summary.append(dic_obj)
-        # Generate PDF report card using the summary with unique task ID
-        pdf_file_link = generate_report_card(
-            f'report_{generate_pdf_report.request.id}.pdf', summary)
-        # Save the report
-        report = Report.objects.create(
-            task_id=generate_pdf_report.request.id, pdf_content=pdf_file_link)
-        return report.id
-    except Exception as e:
-        error_message = str(e)
-        report = Report.objects.create(
-            task_id=generate_pdf_report.request.id,
-            status="FAILURE"
-        )
-        return error_message
+    time.sleep(60)  # Simulate a long-running task
+    # Parse events and prepare data
+    summary = []
+    for each in data:
+        dic_obj = {}
+        dic_obj['student_id'] = each['student_id']
+        event_order = get_event_sorted_alis(each['events'])
+        dic_obj['event_order'] = event_order
+        summary.append(dic_obj)
+    # Generate PDF report card using the summary with unique task ID
+    pdf_file_link = generate_report_card(
+        f'report_{generate_pdf_report.request.id}.pdf', summary)
+    # Save the report
+    return {"pdf_file_link": pdf_file_link, "type": "pdf"}
